7a80d1db2bc296142b7cf36e9eb114283aed45d7
vigneshvetrivel8 <vigneshvetrivel88@gmail.com>
2025-09-21 05:02:50 +0000

    Reduced trading fee

--- pumplistener/trade.py
+++ pumplistener/trade.py
@@ -26,7 +26,7 @@ def buy(public_key, private_key, mint_address, rpc_url):
             "slippage": 15,
             # "priorityFee": 0.0001,
             # "priorityFee": 0.00025,
-            "priorityFee": 0.001,
+            "priorityFee": 0.0001,
             "pool": "auto"
         })
         response.raise_for_status() # Raise an exception for bad status codes
@@ -80,7 +80,7 @@ def sell(public_key, private_key, mint_address, rpc_url):
         "amount": "100%",
         "denominatedInSol": "false",
         "slippage": 99,
-        "priorityFee": 0.0001,
+        "priorityFee": 0.00001,
         # "priorityFee": 0.00025,
         "pool": "auto"
     })
