da88564f1e450912b935af143ec4ad7b27a8272d
vigneshvetrivel8 <vigneshvetrivel88@gmail.com>
2025-10-02 05:11:15 +0000

    Experiment

--- pumplistener/trade4.py
+++ pumplistener/trade4.py
@@ -147,8 +147,9 @@ async def buy(PUBLIC_KEY, PRIVATE_KEY, MINT_ADDRESS, RPC_URL):
     """Execute Pump.fun trade, now async."""
     use_jito=True, 
     # jito_tip_lamports=100000 # this is equivalent to $0.02 only 
-    jito_tip_lamports=150000 # this is equivalent to $0.03 only
+    # jito_tip_lamports=150000 # this is equivalent to $0.03 only
     # we may require $0.2 
+    jito_tip_lamports=1000000
     keypair = Keypair.from_base58_string(PRIVATE_KEY)
     print("="*80)
     print("PUMP.FUN PARALLEL JITO BUNDLE TRADER")
