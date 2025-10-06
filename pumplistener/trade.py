9c82434adc6154b3a4bc966ca927530f873e15c0
vigneshvetrivel8 <vigneshvetrivel88@gmail.com>
2025-10-06 07:06:07 +0000

    Bit reduced priority fee

--- pumplistener/trade.py
+++ pumplistener/trade.py
@@ -26,7 +26,7 @@ def buy(public_key, private_key, mint_address, rpc_url):
             "slippage": 20,
             # "slippage": 50,
             # "priorityFee": 0.00001,
-            "priorityFee": 0.00025,
+            "priorityFee": 0.0001,
             "pool": "auto"
         })
         response.raise_for_status() # Raise an exception for bad status codes
